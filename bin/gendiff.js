deca720a0256c360e92acda79210fab780bb2cb5
Vladimir Basalaev <cautionl@mail.ru>
2020-09-08 21:55:15 +0300

    Step №2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,11 +3,11 @@
 import program from 'commander';
 
 program
-  .description('Compares two configuration files and shows a difference.');
+  .description('Compares two configuration files and shows a difference.')
+  .helpOption('-h, --help', 'read more information')
+  .option('-f, --format [type]', 'output format')
+  .arguments('<filepath1> <filepath2>');
 
 program.version('0.0.1', '-v, --version', 'output the version number');
-
-program
-  .helpOption('-h, --help', 'read more information');
 
 program.parse(process.argv);
