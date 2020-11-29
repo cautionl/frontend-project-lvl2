b7230fb6f01c3d398cf91cc2d7ac7809e2c49910
Vladimir Basalaev <cautionl@mail.ru>
2020-11-29 23:24:08 +0300

    Fix stylish.js

--- src/ formatters /stylish.js
+++ src/ formatters /stylish.js
@@ -3,7 +3,7 @@ import _ from 'lodash';
 const smallIdent = 2;
 const indent = 4;
 
-const createIndent = (depth) => {
+const buildIndent = (depth) => {
   const indentCount = (depth * indent) + smallIdent;
   return ' '.repeat(indentCount);
 };
@@ -15,10 +15,9 @@ const stringify = (data, depth) => {
 
   const keys = Object.keys(data);
 
-  const result = keys.map((key) => `${createIndent(depth + 1)}  ${key}: ${stringify(data[key], depth + 1)}`);
+  const result = keys.map((key) => `${buildIndent(depth + 1)}  ${key}: ${stringify(data[key], depth + 1)}`);
 
-  const attachmentMargins = ' '.repeat((depth + 1) * indent);
-  return `{\n${result.join('\n')}\n${attachmentMargins}}`;
+  return `{\n${result.join('\n')}\n${' '.repeat((depth + 1) * indent)}}`;
 };
 
 const renderStylish = (tree) => {
@@ -26,25 +25,24 @@ const renderStylish = (tree) => {
     const result = data.map((noda) => {
       switch (noda.type) {
         case 'added':
-          return `${createIndent(depth)}+ ${noda.key}: ${stringify(noda.value, depth)}`;
+          return `${buildIndent(depth)}+ ${noda.key}: ${stringify(noda.value, depth)}`;
         case 'removed':
-          return `${createIndent(depth)}- ${noda.key}: ${stringify(noda.value, depth)}`;
+          return `${buildIndent(depth)}- ${noda.key}: ${stringify(noda.value, depth)}`;
         case 'changed': {
-          const oldValue = `${createIndent(depth)}- ${noda.key}: ${stringify(noda.oldValue, depth)}`;
-          const newValue = `${createIndent(depth)}+ ${noda.key}: ${stringify(noda.newValue, depth)}`;
+          const oldValue = `${buildIndent(depth)}- ${noda.key}: ${stringify(noda.oldValue, depth)}`;
+          const newValue = `${buildIndent(depth)}+ ${noda.key}: ${stringify(noda.newValue, depth)}`;
           return `${oldValue}\n${newValue}`;
         }
         case 'unchanged':
-          return `${createIndent(depth)}  ${noda.key}: ${stringify(noda.value, depth)}`;
+          return `${buildIndent(depth)}  ${noda.key}: ${stringify(noda.value, depth)}`;
         case 'nested':
-          return `${createIndent(depth)}  ${noda.key}: ${iter(noda.children, depth + 1)}`;
+          return `${buildIndent(depth)}  ${noda.key}: ${iter(noda.children, depth + 1)}`;
         default:
           throw new Error(`${noda.type} is not defined`);
       }
     });
 
-    const attachmentMargins = ' '.repeat(depth * indent);
-    return `{\n${result.join('\n')}\n${attachmentMargins}}`;
+    return `{\n${result.join('\n')}\n${' '.repeat(depth * indent)}}`;
   };
 
   return iter(tree, 0);
